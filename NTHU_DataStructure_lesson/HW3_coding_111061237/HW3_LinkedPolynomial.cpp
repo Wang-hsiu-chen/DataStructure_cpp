9f157eb9037c67d81cb1f4dacd3c693bf9610972
TWang <wang.tw.ted@gmail.com>
2024-05-11 17:09:28 +0800

    update 3-3

--- NTHU_DataStructure_lesson/HW3_coding_111061237/HW3_LinkedPolynomial.cpp
+++ NTHU_DataStructure_lesson/HW3_coding_111061237/HW3_LinkedPolynomial.cpp
@@ -1,6 +1,20 @@
 #include "HW3_LinkedPolynomial.h"
 
-Polynomial::operator+(const Polynomial & b) const
+Polynomial::Polynomial(const Polynomial &a)
+{
+    head = NULL;
+};
+Polynomial::~Polynomial()
+{
+    while (head != NULL)
+    {
+        Term *temp = head->link;
+        delete head;
+        if (head != NULL)
+            head = temp->link;
+    }
+}
+Polynomial Polynomial::operator+(const Polynomial &b) const
 {
     Term temp;
     Chain<Term>::ChainIterator ai = poly.begin(), bi = b.poly.begin();
@@ -37,4 +51,130 @@ Polynomial::operator+(const Polynomial & b) const
         bi++;
     }
     return c;
+}
+Polynomial Polynomial::operator-(const Polynomial &b) const
+{
+    Term temp;
+    Chain<Term>::ChainIterator ai = poly.begin(), bi = b.poly.begin();
+    Polynomial c;
+    while (ai && bi)
+    {
+        if (ai->exp == bi->exp)
+        {
+            int sum = ai->coef - bi->coef;
+            if (sum)
+                c.poly.InsertBack(temp.Set(sum, ai->exp));
+            ai++;
+            bi++;
+        }
+        else if (ai->exp < bi->exp)
+        {
+            c.poly.InsertBack(temp.Set(-1 * bi->coef, bi->exp));
+            bi++;
+        }
+        else
+        {
+            c.poly.InsertBack(temp.Set(ai->coef, ai->exp));
+            ai++;
+        }
+    }
+    while (ai)
+    {
+        c.poly.InsertBack(temp.Set(ai->coef, ai->exp));
+        ai++;
+    }
+    while (bi)
+    {
+        c.poly.InsertBack(temp.Set(-1 * bi->coef, bi->exp));
+        bi++;
+    }
+    return c;
+}
+Polynomial Polynomial::operator*(const Polynomial &b) const
+{
+    Term temp;
+    Chain<Term>::ChainIterator ai = poly.begin(), bi = b.poly.begin();
+    Polynomial d;
+    for (int i = 0; i < poly.Size(); i++)
+    {
+        Polynomial c;
+        for (int j = 0; j < b.poly.Size(); j++)
+        {
+            int t = ai->coef * bi->coef;
+            if (t)
+                c.poly.InsertBack(temp.Set(t, ai->exp + bi->exp));
+        }
+        d = d + c;
+    }
+    return *this;
+}
+double Polynomial::Evaluate(double x) const
+{
+    Chain<Term>::ChainIterator ai = poly.begin();
+    int leadExp = ai->exp;
+    int y = Coef(leadExp);
+    for (int i = 1; i < leadExp; i++)
+        y = f * y + Coef(leadExp - i);
+    return y;
+}
+int Polynomial::Coef(int e)
+{
+    Chain<Term>::ChainIterator ai = poly.begin();
+    for (int i = 0; i < poly.Size(); i++)
+    {
+        if (ai.exp == e)
+            return ai.coef;
+        ai++;
+    }
+    return 0;
+}
+int Polynomial::LeadExp()
+{
+    Chain<Term>::ChainIterator ai = poly.begin();
+    int leadExp = ai->exp;
+    if (terms <= 1)
+        return leadExp;
+    for (int i = 1; i < terms; i++)
+    {
+        if (ai->exp > leadExp)
+            leadExp = ai->exp;
+        ai++;
+    }
+    return leadExp;
+}
+istream &operator>>(istream &ins, Polynomial &arg)
+{
+    char oper, plus = '+', lastPlus;
+    int coefficient = 0;
+    int exponential = 0;
+    while (plus == '+' || plus == '-')
+    {
+        lastPlus = plus;
+        ins >> coefficient >> oper >> exponential;
+        if (lastPlus == '-')
+            arg.poly.InsertBack(-coefficient, exponential);
+        else
+            arg.poly.InsertBack(coefficient, exponential);
+        do
+        {
+            cin.get(plus);
+        } while (plus == ' ');
+    }
+    return ins;
+}
+ostream &operator<<(ostream &outs, Polynomial &arg)
+{
+    arg.Coef();
+    Chain<Term>::ChainIterator ai = poly.begin();
+    outs << arg.Coef(arg.LeadExp()) << 'x' << arg.LeadExp();
+    if (arg.LeadExp() == 0)
+        return outs;
+    for (int i = arg.LeadExp() - 1; i >= 0; i--)
+    {
+        if (arg.Coef(i) > 0)
+            outs << " + " << arg.Coef(i) << 'x' << i;
+        else if (arg.Coef(i) < 0)
+            outs << " - " << abs(arg.Coef(i)) << 'x' << i;
+    }
+    return outs;
 }
--- NTHU_DataStructure_lesson/HW3_coding_111061237/HW3_LinkedPolynomial.h
+++ NTHU_DataStructure_lesson/HW3_coding_111061237/HW3_LinkedPolynomial.h
@@ -1,29 +1,114 @@
 #include <iostream>
 using namespace std;
 
-struct Term
+class Polynomial; // 前向宣告
+class Term
 {
+    friend class Polynomial;
+
+private:
     int coef;
     int exp;
-    Term Set(int c, int e)
+    Term *link;
+    inline Term Set(int c, int e)
     {
         coef = c;
         exp = e;
         return *this;
     }
+    inline Term GetCoef() { return coef; }
+    inline Term GetExp() { return exp; }
+};
+template <class T>
+class Chain
+{
+public:
+    int Size()
+    {
+        int size = 0;
+        if (head == NULL)
+            return 0;
+        Term *pointer = head;
+        while (pointer->link != head)
+        { // delete first
+            size++;
+            pointer = pointer->link;
+        }
+        return size + 1;
+    }
+    void InsertBack(const T &node)
+    {
+        if (head == NULL) // empty list
+        {
+            head = node;
+            head->link = head;
+        }
+        else if (head != NULL)
+        {
+            Term *temp = head;
+            while (temp->link != head)
+                temp = temp->link;
+            node->link = temp->link;
+            temp->link = node;
+        }
+    }
+    class ChainIterator
+    {
+    public: // typedefs required by C++ for a forward iterator
+        // Constructor
+        ChainIterator(Term *startNode = 0)
+        {
+            current = startNode;
+        }
+        // Dereferencing operators
+        T &operator*() const { return current; }
+        T *operator->() const { return &current; }
+        // Increment
+        ChainIterator &operator++() // preincrement
+        {
+            current = current->link;
+            return *this;
+        }
+        ChainIterator &operator++(int) // postincrement
+        {
+            ChainIterator old = *this;
+            current = current->link;
+            return old;
+        }
+        // Equality test
+        bool operator!=(const ChainIterator r)
+        {
+            return current != r.current;
+        }
+        bool operator==(const ChainIterator r)
+        {
+            return current == r.current;
+        }
+
+    private:
+        Term *current;
+    };
+    ChainIterator begin() { return ChainIterator(first); }
+    ChainIterator end() { return ChainIterator(0); }
+
+private:
+    Term *head;
 };
 class Polynomial
 {
 public:
-    Istream &operator>>(istream &is, Polynomial &x);                    // Read in an input polynomial and convert it to its circular list representation using a header node.
-    Ostream &operator<<(ostream &os, Polynomial &x);                    // Convert x from its linked list representation to its external representation and output it.
-    Polynomila::Polynomial(const Polynomial &a);                        // copy constructor
-    Const Polynomila &Polynomial::operator=(const Polynomial &a) const; // assign polynomial a to *this.
-    Polynomial::~Polynomial();                                          // desctructor, return all nodes to available-space list
-    Polynomial operator+(const Polynomial &b) const;                    //  Create and return the polynomial *this + b
-    Polynomial operator-(const Polynomial &b) const;                    //  Create and return the polynomial *this – b
-    Polynomial operator*(const Polynomial &b) const;                    //  Create and return the polynomial *this * b
-    double Polynomial::Evaluate(double x) const;                        // Evaluate the polynomial *this and return the result.
+    friend istream &operator>>(istream &is, Polynomial &x); // Read in an input polynomial and convert it to its circular list representation using a header node.
+    friend ostream &operator<<(ostream &os, Polynomial &x); // Convert x from its linked list representation to its external representation and output it.
+    Polynomial::Polynomial(const Polynomial &a);            // copy constructor
+    Polynomial::~Polynomial();
+    const Polynomila& Polynomial::operator=(const Polynomial& a) const[assignment operator]; // assign polynomial a to *this.
+    Polynomial::~Polynomial();                                                               // desctructor, return all nodes to available-space list
+    Polynomial operator+(const Polynomial &b) const;                                         //  Create and return the polynomial *this + b
+    Polynomial operator-(const Polynomial &b) const;                                         //  Create and return the polynomial *this – b
+    Polynomial operator*(const Polynomial &b) const;                                         //  Create and return the polynomial *this * b
+    double Polynomial::Evaluate(double x) const;                                             // Evaluate the polynomial *this and return the result.
+    int Polynomial::Coef(int e);
+    int Polynomial::LeadExp();
 
 private:
     Chain<Term> poly;
