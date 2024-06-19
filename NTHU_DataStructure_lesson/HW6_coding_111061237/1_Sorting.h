d7ee9ba07a411b3a515d722308bbb14cb4095a9b
TWang <wang.tw.ted@gmail.com>
2024-06-19 22:14:20 +0800

    add ADT

---
+++ NTHU_DataStructure_lesson/HW6_coding_111061237/1_Sorting.h
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class Sorting
+{
+public:
+    Sorting(){};
+    void InsertionSort(T *a, const int n);
+    void QuickSort(T *a, const int n);
+    void MergeSort(T *a, const int n);
+    void RecursiveMergeSort(T *a, const int n);
+    void HeapSort(T *a, const int n);
+    void RadixSort();
+
+private:
+};
