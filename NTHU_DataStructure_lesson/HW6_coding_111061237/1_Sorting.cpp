799039c1768ad27c78d8b6b5d265d68e2fd25ca3
TWang <wang.tw.ted@gmail.com>
2024-06-19 22:32:02 +0800

    add quick sort

--- NTHU_DataStructure_lesson/HW6_coding_111061237/1_Sorting.cpp
+++ NTHU_DataStructure_lesson/HW6_coding_111061237/1_Sorting.cpp
@@ -17,3 +17,28 @@ void Sorting<T>::InsertionSort(T *a, const int n)
         cout << a[i] << ", ";
     cout << endl;
 }
+
+template <class T>
+void Sorting<T>::QuickSort(T *a, const int left, const int right)
+{ // sort a[left:right] into nondecreasing, a[left] as pivot
+    if (left < right)
+    {
+        T &pivot = a[left];
+        int i = left;
+        int j = right + 1;
+        do
+        {
+            do
+                j--;
+            while (a[j] > pivot); // find a key ≤pivot
+            do
+                i++;
+            while (a[i] <= pivot); // find a key >pivot
+            if (i < j)
+                swap(a[i], a[j]);
+        } while (i < j);
+        swap(pivot, a[j]);          // place the pivot between 2 lists
+        QuickSort(a, left, j - 1);  // recursion
+        QuickSort(a, j + 1, right); // recursion
+    }
+}
