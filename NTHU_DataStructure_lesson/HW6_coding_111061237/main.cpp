ad6b91e36825bd7a08bbf47e1772f4268f1d137c
TWang <wang.tw.ted@gmail.com>
2024-06-19 22:31:12 +0800

    construct a input number func quickSort test for int,char,float

--- NTHU_DataStructure_lesson/HW6_coding_111061237/main.cpp
+++ NTHU_DataStructure_lesson/HW6_coding_111061237/main.cpp
@@ -3,6 +3,21 @@
 #include <stdlib.h>
 #include <time.h>
 
+void RamdomInput(int *intList, char *charList, float *floatList, string *stringList, int numbers)
+{
+    for (int i = 0; i < numbers; i++)
+    {
+        intList[i] = rand() % 100;
+        char correspondingChar = 'a' + (rand() % 26);
+        charList[i] = correspondingChar;
+        floatList[i] = rand() % 100 + (rand() % 100) / 100.0;
+        for (int j = 0; j < 4; j++)
+        {
+            char correspondingChar = 'a' + (rand() % 26);
+            stringList[i] += correspondingChar;
+        }
+    }
+}
 int main()
 {
     Sorting<int> A;
@@ -16,22 +31,25 @@ int main()
     string *stringList = new string[numbers];
 
     srand(time(NULL));
-    for (int i = 0; i < numbers; i++)
-    {
-        intList[i] = rand() % 100;
-        char correspondingChar = 'a' + (rand() % 26);
-        charList[i] = correspondingChar;
-        floatList[i] = rand() % 100 + (rand() % 100) / 100.0;
-        for (int j = 0; j < 4; j++)
-        {
-            char correspondingChar = 'a' + (rand() % 26);
-            stringList[i] += correspondingChar;
-        }
-    }
+    RamdomInput(intList, charList, floatList, stringList, numbers);
     for (int i = 0; i < numbers; i++)
         cout << intList[i] << " " << charList[i] << " " << floatList[i] << " " << stringList[i] << endl;
 
     A.InsertionSort(intList, numbers);
     B.InsertionSort(charList, numbers);
     C.InsertionSort(floatList, numbers);
+
+    RamdomInput(intList, charList, floatList, stringList, numbers);
+    A.QuickSort(intList, 0, numbers - 1);
+    for (int i = 0; i < numbers; i++)
+        cout << intList[i] << ", ";
+    cout << endl;
+    B.QuickSort(charList, 0, numbers - 1);
+    for (int i = 0; i < numbers; i++)
+        cout << charList[i] << ", ";
+    cout << endl;
+    C.QuickSort(floatList, 0, numbers - 1);
+    for (int i = 0; i < numbers; i++)
+        cout << floatList[i] << ", ";
+    cout << endl;
 }
