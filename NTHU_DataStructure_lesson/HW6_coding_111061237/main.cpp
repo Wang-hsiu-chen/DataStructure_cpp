dcf067bf679457e9dcf206dbc95c224ed0299eec
TWang <wang.tw.ted@gmail.com>
2024-06-23 03:54:30 +0800

    revise IO revise formet add test for all sorts add test for 2 ht

--- NTHU_DataStructure_lesson/HW6_coding_111061237/main.cpp
+++ NTHU_DataStructure_lesson/HW6_coding_111061237/main.cpp
@@ -6,6 +6,8 @@
 void RamdomInput(int *intList, char *charList, float *floatList, string *stringList, int numbers)
 {
     for (int i = 0; i < numbers; i++)
+        stringList[i] = "";
+    for (int i = 0; i < numbers; i++)
     {
         intList[i] = rand() % 100;
         char correspondingChar = 'a' + (rand() % 26);
@@ -17,6 +19,19 @@ void RamdomInput(int *intList, char *charList, float *floatList, string *stringL
             stringList[i] += correspondingChar;
         }
     }
+    cout << "<<unsort lists>>" << endl;
+    for (int i = 0; i < numbers; i++)
+        cout << intList[i] << ", ";
+    cout << endl;
+    for (int i = 0; i < numbers; i++)
+        cout << charList[i] << ", ";
+    cout << endl;
+    for (int i = 0; i < numbers; i++)
+        cout << floatList[i] << ", ";
+    cout << endl;
+    for (int i = 0; i < numbers; i++)
+        cout << stringList[i] << ", ";
+    cout << endl;
 }
 int main()
 {
@@ -24,32 +39,114 @@ int main()
     Sorting<char> B;
     Sorting<float> C;
     Sorting<string> D;
-    int numbers = 5;
+    int numbers = 20;
     int *intList = new int[numbers];
     char *charList = new char[numbers];
     float *floatList = new float[numbers];
     string *stringList = new string[numbers];
-
     srand(time(NULL));
-    RamdomInput(intList, charList, floatList, stringList, numbers);
-    for (int i = 0; i < numbers; i++)
-        cout << intList[i] << " " << charList[i] << " " << floatList[i] << " " << stringList[i] << endl;
 
+    cout << "    Sorting" << endl;
+    RamdomInput(intList, charList, floatList, stringList, numbers);
+    cout << "  <<Insertion Sort>>" << endl;
     A.InsertionSort(intList, numbers);
     B.InsertionSort(charList, numbers);
     C.InsertionSort(floatList, numbers);
+    D.InsertionSort(stringList, numbers);
 
+    cout << endl;
     RamdomInput(intList, charList, floatList, stringList, numbers);
+    cout << "  <<Quick Sort>>" << endl;
     A.QuickSort(intList, 0, numbers - 1);
+    B.QuickSort(charList, 0, numbers - 1);
+    C.QuickSort(floatList, 0, numbers - 1);
+    D.QuickSort(stringList, 0, numbers - 1);
     for (int i = 0; i < numbers; i++)
         cout << intList[i] << ", ";
     cout << endl;
-    B.QuickSort(charList, 0, numbers - 1);
     for (int i = 0; i < numbers; i++)
         cout << charList[i] << ", ";
     cout << endl;
-    C.QuickSort(floatList, 0, numbers - 1);
     for (int i = 0; i < numbers; i++)
         cout << floatList[i] << ", ";
     cout << endl;
+    for (int i = 0; i < numbers; i++)
+        cout << stringList[i] << ", ";
+
+    cout << endl;
+    cout << endl;
+    RamdomInput(intList, charList, floatList, stringList, numbers);
+    cout << "  <<Merge Sort>>" << endl;
+    A.MergeSort(intList, numbers);
+    B.MergeSort(charList, numbers);
+    C.MergeSort(floatList, numbers);
+
+    cout << endl;
+    RamdomInput(intList, charList, floatList, stringList, numbers);
+    cout << "  <<Recursive Merge Sort>>" << endl;
+    A.RecursiveMergeSort(intList, 0, numbers - 1);
+    B.RecursiveMergeSort(charList, 0, numbers - 1);
+    C.RecursiveMergeSort(floatList, 0, numbers - 1);
+    D.RecursiveMergeSort(stringList, 0, numbers - 1);
+    for (int i = 0; i < numbers; i++)
+        cout << intList[i] << ", ";
+    cout << endl;
+    for (int i = 0; i < numbers; i++)
+        cout << charList[i] << ", ";
+    cout << endl;
+    for (int i = 0; i < numbers; i++)
+        cout << floatList[i] << ", ";
+    cout << endl;
+    for (int i = 0; i < numbers; i++)
+        cout << stringList[i] << ", ";
+
+    cout << endl;
+    cout << endl;
+    RamdomInput(intList, charList, floatList, stringList, numbers);
+    cout << "  <<Heap Sort>>" << endl;
+    A.HeapSort(intList, numbers);
+    B.HeapSort(charList, numbers);
+    C.HeapSort(floatList, numbers);
+    D.HeapSort(stringList, numbers);
+    for (int i = 0; i < numbers; i++)
+        cout << intList[i] << ", ";
+    cout << endl;
+    for (int i = 0; i < numbers; i++)
+        cout << charList[i] << ", ";
+    cout << endl;
+    for (int i = 0; i < numbers; i++)
+        cout << floatList[i] << ", ";
+    cout << endl;
+    for (int i = 0; i < numbers; i++)
+        cout << stringList[i] << ", ";
+    cout << endl;
+
+    LinearProbing<string> d1;
+    Chaining<string> d2;
+    const string keys[] = {"abc", "abcde", "b", "bye", "cool", "dit", "fox", "vivy", "yes", "hello"};
+
+    cout << endl
+         << "    Hashing" << endl
+         << "  <<Insert keys>>" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        d1.Insert(keys[i]);
+        d2.Insert(keys[i]);
+    }
+
+    cout << "  <<get d1>>" << endl;
+    for (int i = 0; i < 10; i++)
+        cout << d1.Get(keys[i]) << ", ";
+    cout << endl;
+    cout << "find a key \"something\" not in d1: " << endl;
+    cout << d1.Get("something") << endl;
+
+    cout << "  <<get d2>>" << endl;
+    for (int i = 0; i < 10; i++)
+        cout << d2.Get(keys[i]) << ", ";
+    cout << endl;
+    cout << "find a key \"something\" not in d2" << endl;
+    cout << d2.Get("something") << endl;
+
+    return 0;
 }
